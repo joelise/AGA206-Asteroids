cddde02e4c9ec3ea613e887c91afa8f1693f3df8
joelise <jo.soyke@gmail.com>
2025-07-23 22:09:09 +1000

    Updated Camera Shake

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -10,40 +10,40 @@ public class CameraShake : MonoBehaviour
 
     void Start()
     {
-       // StartCoroutine(ShakeRoutine());
+        StartCoroutine(ShakeRoutine());
+       
     }
 
     public IEnumerator ShakeRoutine()
     {
         float timer = 0f;
-        float t = 0f;
+        float t = 2f;
         Vector3 originalPos = transform.position;
-        Vector3 targetPos = originalPos;
-        targetPos.x += Random.Range(Offset, -Offset);
-        targetPos.y += Random.Range(Offset, -Offset);
-        Vector3 newPos = targetPos;
-        
-
-        while(t < 1f)
+        while (t < 5f)
         {
+           // Vector3 originalPos = transform.position;
+            Vector3 targetPos = originalPos;
+            targetPos.x += Random.Range(Offset, -Offset);
+            targetPos.y += Random.Range(Offset, -Offset);
+            Vector3 newPos = targetPos;
+
             timer += Time.deltaTime;
             t = Mathf.Clamp01(timer / ShakeDuration);
             transform.position = Vector3.Lerp(originalPos, newPos, t);
             transform.position = Vector3.Lerp(newPos, originalPos, t);
-            
-            
+            transform.position = Vector3.Lerp(originalPos, newPos, t);
+            Debug.Log(timer);
 
             yield return new WaitForEndOfFrame();
-            
-
         }
-        
-
+       
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-            StartCoroutine(ShakeRoutine());
+        //if (Input.GetKeyDown(KeyCode.P))
+        //StartCoroutine(ShakeRoutine());
+
+        
     }
 }
